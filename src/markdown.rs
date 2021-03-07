f28c957e1b2e5e8e4a1d403131995ba692cd59ee
Wonko <wonko@hanstool.org>
2021-03-07 01:38:20 +0100

    markdown parser: statemachine to parse

--- src/markdown.rs
+++ src/markdown.rs
@@ -15,6 +15,8 @@ lazy_static! {
     static ref REQUIREMENT_LINE: Regex =
         Regex::new(r"^(#+)\s*([A-Za-z][a-zA-Z0-9_]+[a-zA-Z0-9]):\s*(.+)\s*$").unwrap();
     static ref ATTRIBUTE_LINE: Regex = Regex::new(r"^([A-Z][a-z]+):\s(.*)$").unwrap();
+    static ref REF_LINK_LINE: Regex = 
+        Regex::new(r"^*\s+([A-Za-z][a-zA-Z0-9_]+[a-zA-Z0-9]):\s*(.+)\s*$").unwrap();
     static ref BAD_HEADLINE_UNDERLINE: Regex = Regex::new(r"^(====*)|(----*)").unwrap(); // TODO: use
 }
 
@@ -41,6 +43,9 @@ pub enum MarkdownParserError {
 
     #[error("File Read error")]
     IOError(PathBuf, io::Error),
+
+    #[error("End of File internal Error")]
+    EOF,
 }
 use MarkdownParserError::*;
 
@@ -74,10 +79,13 @@ impl Artefact for MarkdownArtefact<'_> {
 }
 
 #[derive(Debug, PartialEq)]
-enum MarkdownParserState {
+enum MarkdownParserState<'a> {
     LookForReq,
+    LookForDesc,
     CollectDesc,
-    CollectAttr,
+    LookForAttr,
+    CollectTextAttr(&mut String),
+    CollectRefLink(&mut Vec),
 }
 use MarkdownParserState::*;
 
@@ -86,8 +94,14 @@ pub struct MarkdownParser<R: io::Read> {
     reader: io::BufReader<R>,
     path: PathBuf,
     line_number: u32,
-    line: Option<String>,
+    line_buffer: Option<String>,
+
+    req : Requirement,
+    level:usize = 0,
+    state:MarkdownParserState,
 }
+
+
 
 impl<R: io::Read> MarkdownParser<R> {
     fn new(read: R, path: PathBuf) -> Self {
@@ -99,106 +113,150 @@ impl<R: io::Read> MarkdownParser<R> {
         }
     }
 
-    fn parse_next(&mut self) -> Result<Option<Requirement>, MarkdownParserError> {
-        let mut req = Requirement::default();
-        let mut level = 0;
-        let mut line_buffer: String = String::default();
-        let mut state = LookForReq;
+    fn location(&self) -> Location {
+        Location::new(self.path.clone(), self.line_number),
+    }
 
-        let mut currently_appending_to: Option<&mut String> = None;
+    fn peek(&mut self) -> &String {
+        self.line_buffer.as_ref().unwrap()
+    }
 
-        let consume = loop {
-            if let Some(l) = self.line.take() {
-                line_buffer = l;
-            } else {
-                line_buffer.clear();
-                let bytes_read = self
-                    .reader
-                    .read_line(&mut line_buffer)
-                    .map_err(|e| IOError(self.path.clone(), e))?;
-                if bytes_read == 0 {
-                    break false;
-                }
-                self.line_number += 1;
+    fn consume(&mut self) -> String {
+        self.line_buffer.take().unwrap()
+    }
+
+
+    fn parse_paragraph(&self, dest: &mut String) -> Result<bool, MarkdownParserError> {
+        let line = self.peek();
+        if let Some(heading_line) = HEADING_LINE.captures(line) {
+            if REQUIREMENT_LINE.is_match(&line) {
+                return true;
             }
-
-            let mut line = line_buffer.as_str();
-
-            if state == LookForReq {
-                if let Some(req_line) = REQUIREMENT_LINE.captures(line) {
-                    level = req_line[1].len();
-                    req.id = req_line[2].to_owned();
-                    req.title = Some(req_line[3].trim().to_owned());
-                    req.location.file = self.path.clone();
-                    req.location.line = self.line_number;
-                    state = CollectDesc;
-
-                    currently_appending_to = None;
-                }
+            self.line_level = heading_line[1].len();
+            if self.line_level <= level {
+                return true;
             } else {
-                if let Some(heading_line) = HEADING_LINE.captures(line) {
-                    if REQUIREMENT_LINE.is_match(&line) {
-                        break false;
-                    }
-                    let line_level = heading_line[1].len();
-                    if line_level <= level {
-                        break true;
-                    } else {
-                        line = line[level..].trim_start();
-                    }
-                }
+                line = line[level..].trim_start();
+            }
+        } else if ATTRIBUTE_LINE.is_match(line) {
+            return self.parse_attr();
+        }
 
-                if let Some(attr_line) = ATTRIBUTE_LINE.captures(line) {
-                    let key = &attr_line[1];
-                    let val = &attr_line[2];
+        dest += line;
+        self.consume();
+        return false
+    }
 
-                    if key == "Tags" {
-                        req.tags = val.split(",").map(|s| s.trim().to_owned()).collect();
-                    } else if key == "Covers" {
-                        req.covers = val.split(",").map(|s| Reference{id:s.trim().to_owned(), title:None}).collect();
-                    } else if key == "Depends" {
-                        req.depends = val.split(",").map(|s| Reference{id:s.trim().to_owned(), title:None}).collect();
-                    } else {
-                        let e = req.attributes.entry(key.to_owned());
+    fn parse_attr(&self) -> Result<bool, MarkdownParserError> {
+        let line = self.consume();
 
-                        match e {
-                            std::collections::hash_map::Entry::Occupied(_) => {
-                                return Err(DiplicateAttribute(
+        if let Some(attr_line) = ATTRIBUTE_LINE.captures(&line) {
+            let key = &attr_line[1];
+            let val = &attr_line[2];
+
+            match key {
+              // TODO  "Tags" => self.parse_tags(val);
+                "Depends" => self.parse_link_attr(&mut self.depends, val);
+                "Covers" => self.parse_link_attr(&mut self.covers, val);
+                _ => {
+                    let e = self.req.attributes.entry(key.to_owned());
+
+                    match e {
+                        std::collections::hash_map::Entry::Occupied(_) => {
+                            return Err(DiplicateAttribute(
                                     Location::new(self.path.clone(), self.line_number),
                                     key.to_owned(),
-                                ));
-                            }
-                            std::collections::hash_map::Entry::Vacant(_) => {
-                                currently_appending_to = Some(e.or_insert(val.to_owned()));
-                            }
+                            ));
                         }
-                    }
-                    state = CollectAttr;
-                } else {
-                    if let Some(ap) = currently_appending_to.as_mut() {
-                        if ap.trim().is_empty() || !line.is_empty() {
-                            ap.push_str(line);
-                        }
-                    } else {
-                        assert!(state == CollectDesc);
-                        if !line.is_empty() {
-                            req.description = Some(mem::take(&mut line_buffer));
-                            currently_appending_to = req.description.as_mut();
+                        std::collections::hash_map::Entry::Vacant(_) => {
+                            self.state = CollectTextAttr(e.or_insert(val.to_owned()));
                         }
                     }
                 }
+        } else {
+            Err(FormatError(self.location(), "Expected an Attribute header"));
+        }
+    }
+
+    fn parse_link_attr(&self, &mut refVec, shortList:String) -> Result<bool, MarkdownParserError> {
+        state = CollectRefLink(refVec);
+
+        for id in shortList.split(",") {
+            id = id.trim();
+            refVec.push(Reference{id, title:None});
+        }
+        return Ok(false);
+    }
+
+    fn parse_ref_link(&self, &mut refVec) -> Result<bool, MarkdownParserError> {
+        let line = self.peek();
+        if let Some(ref_link) = REF_LINK_LINE.captures(line) {
+            let id = ref_link[1].to_owned();
+            let title = ref_link[2];
+
+            let title = if title.is_empty {
+                None
+            } else {
+                Some(title)
+            }
+
+            refVec.push(Reference{...});
+        }
+    }
+
+    fn parse_next(&mut self) -> Result<Option<Requirement>, MarkdownParserError> {
+        loop {
+            let line : &str;
+            if let Some(l) = self.line_buffer.as_ref() {
+                line = l;
+            } else {
+                let buffer = String::new();
+                let bytes_read = self
+                    .reader
+                    .read_line(&mut buffer)
+                    .map_err(|e| IOError(self.path.clone(), e))?;
+                if bytes_read > 0 {
+                    self.line_number += 1;
+                    self.line_buffer = Some(buffer);
+                    line = self.line_buffer.as_ref().unwrap());
+                } else {
+                    break true;
+                }
+            }
+
+            let done = match state {
+                LookForReq =>  {
+                    let l = self.consume();
+                    if let Some(req_line) = REQUIREMENT_LINE.captures(&l) {
+                        self.level = req_line[1].len();
+                        req.id = req_line[2].to_owned();
+                        req.title = Some(req_line[3].trim().to_owned());
+                        req.location = self.location();
+
+                        self.state = LookForDesc;
+                    }
+
+                    false
+                },
+                LookForDesc => {
+                    let l = self.consume()
+                    if !l.trim().is_empty() {
+                        self.description = Some(l);
+                        self.state =  CollectDesc;
+                    }
+
+                    false
+                },
+                CollectDesc => self.parse_paragraph(self.description.as_mut().unwrap())?,
+                CollectTextAttr(s) => self.parse_paragraph(s)?,
+                CollectRefLink(vec) => self.parse_ref_link(vec)?,
+                _ => {},
+            }
+
+            if done {
+                return Ok(Some(req));
             }
         };
-
-        if !consume {
-            self.line = Some(line_buffer);
-        }
-
-        if state != LookForReq {
-            return Ok(Some(req));
-        } else {
-            return Ok(None);
-        }
     }
 }
 
