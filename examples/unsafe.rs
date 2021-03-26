6b5e5e1b6f5e237e14c46d8eb7a73bde43cc4001
Wonko <wonko@hanstool.org>
2021-03-26 23:44:16 +0100

    unsafe reasoning

--- examples/unsafe.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    // If this file is parsed as a `Rust Unsafe` Artefact, the following line
-    // leads to Requirement with the ID `UNSAFE_EXAMPLE`
-    #[allow(unused_unsafe)]
-    let _x = unsafe /* UNSAFE_EXAMPLE */ { 42 };
-}
--- src/common.rs
+++ src/common.rs
@@ -319,17 +319,17 @@ impl StringVault {
     /// returns a `&str` reference to the interned string
     pub fn keep<'a>(&'a self, val: String) -> &'a str {
         let set = self.set.get();
-        let set = unsafe /* UNSAFE_POOL_MAP */ {
+        let set = unsafe /* UNSAFE_StringVault_keep_set */ {
             set.as_mut()
         };
         let set = set.unwrap();
         let interned;
         if let Some(old) = set.get(&val) {
-            unsafe /* UNSAFE_POOL_OLD */ {
+            unsafe /* UNSAFE_StringVault_keep_old */ {
                 interned = std::mem::transmute::<&'_ str, &'a str>(old.as_str());
             }
         } else {
-            unsafe /* UNSAFE_POOL_NEW */ {
+            unsafe /* UNSAFE_StringVault_keep_new */ {
                 interned = std::mem::transmute::<&'_ str, &'a str>(val.as_str());
             }
             set.insert(val);
@@ -344,7 +344,7 @@ impl StringVault {
     /// Returns a `&str` reference to the interned string
     pub fn keep_cloned<'a, S: AsRef<str>>(&'a self, val: S) -> &'a str {
         let set = self.set.get();
-        let set = unsafe /* UNSAFE_POOL_MAP */ {
+        let set = unsafe /* UNSAFE_StringVault_keep_cloned_set */ {
             set.as_mut()
         };
         let set = set.unwrap();
@@ -353,12 +353,12 @@ impl StringVault {
 
         let interned;
         if let Some(old) = set.get(val) {
-            unsafe /* UNSAFE_POOL_OLD */ {
+            unsafe /* UNSAFE_StringVault_keep_cloned_old */ {
                 interned = std::mem::transmute::<&'_ str, &'a str>(old.as_str());
             }
         } else {
             let val: String = String::from(val);
-            unsafe /* UNSAFE_POOL_NEW */ {
+            unsafe /* UNSAFE_StringVault_keep_cloned_new */ {
                 interned = std::mem::transmute::<&'_ str, &'a str>(val.as_str());
             }
             set.insert(val);
