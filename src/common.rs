e46d62240bc9a32b1a2aa66b35525200fabd00bd
Wonko <wonko@hanstool.org>
2021-03-26 23:24:59 +0100

    String Vault: initial impl

--- src/common.rs
+++ src/common.rs
@@ -1,6 +1,12 @@
-use std::{collections::HashMap, mem};
+use std::cell::UnsafeCell;
+use std::collections::HashMap;
+use std::collections::HashSet;
+use std::fmt;
+use std::fs;
+use std::io;
+use std::mem;
+use std::path::Path;
 use std::path::PathBuf;
-use std::{fmt, fs, io, path::Path};
 
 use crate::parsers::markdown::markdown_parse;
 
@@ -268,5 +274,157 @@ impl<'a> Artefact<'a> {
             }
         }
         return result;
+    }
+}
+
+pub struct StringVault {
+    set: UnsafeCell<HashSet<String>>,
+}
+
+/// Place to put strings so you can point to them from various places.
+/// The `&str` live as long as the StringVault.
+///
+/// # Examples
+/// ```rust
+/// let sv = StringVault::new();
+/// let s:&str = sv.keep("Hello World");
+///
+/// sv.keep(s); // moves s into keep
+///
+/// println!("{}", s);
+/// ```
+///
+/// ```compile_fail
+/// let sv = StringVault::new();
+/// let s:&str = sv.keep("Hello World");
+///
+/// sv.keep(s);
+///
+/// std::mem::drop(sv);
+///
+/// println!("{}", s); // s can not outlive sv
+/// ```
+///
+impl StringVault {
+    /// Create a new empty StringVault
+    pub fn new() -> Self {
+        let set: UnsafeCell<_> = HashSet::new().into();
+        Self { set }
+    }
+
+    /// Move a string into the StringVault.
+    ///
+    /// If an equal string already exists, the new one is droped, otherwise
+    /// the new one is kept.
+    /// returns a `&str` reference to the interned string
+    pub fn keep<'a>(&'a self, val: String) -> &'a str {
+        let set = self.set.get();
+        let set = unsafe /* UNSAFE_POOL_MAP */ {
+            set.as_mut()
+        };
+        let set = set.unwrap();
+        let interned;
+        if let Some(old) = set.get(&val) {
+            unsafe /* UNSAFE_POOL_OLD */ {
+                interned = std::mem::transmute::<&'_ str, &'a str>(old.as_str());
+            }
+        } else {
+            unsafe /* UNSAFE_POOL_NEW */ {
+                interned = std::mem::transmute::<&'_ str, &'a str>(val.as_str());
+            }
+            set.insert(val);
+        }
+        return interned;
+    }
+
+    /// Find an equal string or create a clone and keep it.
+    ///
+    /// if no equal string exists, a new `String` is created and kept.
+    ///
+    /// Returns a `&str` reference to the interned string
+    pub fn keep_cloned<'a, S: AsRef<str>>(&'a self, val: S) -> &'a str {
+        let set = self.set.get();
+        let set = unsafe /* UNSAFE_POOL_MAP */ {
+            set.as_mut()
+        };
+        let set = set.unwrap();
+
+        let val: &str = val.as_ref();
+
+        let interned;
+        if let Some(old) = set.get(val) {
+            unsafe /* UNSAFE_POOL_OLD */ {
+                interned = std::mem::transmute::<&'_ str, &'a str>(old.as_str());
+            }
+        } else {
+            let val: String = String::from(val);
+            unsafe /* UNSAFE_POOL_NEW */ {
+                interned = std::mem::transmute::<&'_ str, &'a str>(val.as_str());
+            }
+            set.insert(val);
+        }
+        return interned;
+    }
+}
+
+/// add `as_interned(&sv)` to `String`
+trait AsInterned {
+    fn as_interned<'a>(self, sv: &'a StringVault) -> &'a str;
+}
+
+impl AsInterned for String {
+    fn as_interned<'a>(self, sv: &'a StringVault) -> &'a str {
+        sv.keep(self)
+    }
+}
+
+/// add `as_interned(&sv)` to anything that can be referenced as `&str`
+trait AsInternedClone {
+    fn as_interned<'a>(&self, sv: &'a StringVault) -> &'a str;
+}
+
+impl<S: AsRef<str>> AsInternedClone for S {
+    fn as_interned<'a>(&self, sv: &'a StringVault) -> &'a str {
+        sv.keep_cloned(self)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn test_string_vault() {
+        let sv = &StringVault::new();
+
+        let s1 = "Hello World".to_string();
+        let s2 = s1.clone();
+
+        let p1 = s1.as_ptr() as usize;
+        let p2 = s2.as_ptr() as usize;
+
+        let si1 = sv.keep(s1);
+        let si2 = sv.keep(s2);
+
+        let pi1 = si1.as_ptr() as usize;
+        let pi2 = si2.as_ptr() as usize;
+
+        assert!(p1 != p2);
+        assert!(pi1 == pi2);
+        assert!(p1 == pi1);
+    }
+
+    #[test]
+    fn test_str_trait() {
+        let sv = StringVault::new();
+
+        let _: &str = "Hello World".as_interned(&sv);
+    }
+
+    #[test]
+    fn test_string_trait() {
+        let sv = StringVault::new();
+        let s = String::from("Hello World");
+
+        let _: &str = s.as_interned(&sv);
     }
 }
