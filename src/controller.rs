378ac029f69bc898ee5436674049bb95dbc3d130
Wonko <wonko@hanstool.org>
2021-10-04 12:17:42 +0200

    configure artefacts and jobs with toml

--- src/controller.rs
+++ src/controller.rs
@@ -4,16 +4,25 @@ use crate::{
     trace::Graph,
 };
 use serde::{Deserialize, Serialize};
-use std::{collections::HashMap, convert::TryFrom, io, path::PathBuf};
+use std::{
+    collections::HashMap,
+    convert::TryFrom,
+    fs::{self, File},
+    io::{self, Write},
+    path::{Path, PathBuf},
+};
+
+use toml;
 
 use crate::errors::{Error, Result};
 use Error::*;
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 struct ArtefactConfigSerialized {
     paths: Vec<PathBuf>,
     parser: String,
-    parser_options: Option<serde_json::Value>,
+    parser_options: Option<String>,
+    version_provider: Option<String>,
 }
 
 impl ArtefactConfigSerialized {
@@ -36,11 +45,24 @@ impl ArtefactConfigSerialized {
     }
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
+struct TraceConfig {
+    upper: String,
+    lower: Vec<String>,
+}
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Config {
-    artefacts: HashMap<String, ArtefactConfigSerialized>,
-    tracing: Vec<(String, Vec<String>)>,
-    jobs: Option<HashMap<String, Job>>,
+    artefact: HashMap<String, ArtefactConfigSerialized>,
+    trace: Vec<TraceConfig>,
+    job: Option<HashMap<String, Job>>,
+    version_provider: Option<String>,
+}
+
+impl Config {
+    pub fn from_toml_file<P: AsRef<Path>>(p: P) -> io::Result<Self> {
+        let config: Config = toml::from_slice(fs::read(p)?.as_slice())?;
+        Ok(config)
+    }
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -51,8 +73,11 @@ pub enum Query {
     /// Query Artefacts and Tracing Edges to see which are outdated
     CacheStatus,
 
-    /// Parse a list of artefacts or all artefacts if empty
-    Parse { artefacts: Vec<String> },
+    /// Parse a list of artefacts
+    ParseArtefacts { artefacts: Vec<String> },
+
+    /// Parse all Artefacts
+    Parse,
 
     /// Trace all Edges in Graph
     Trace,
@@ -68,57 +93,88 @@ pub enum Query {
 pub struct Job {
     pub query: Query,
     pub format: Format,
+    pub file: PathBuf,
 }
 
+#[derive(Debug)]
 pub struct Controller<'config> {
     config: &'config Config,
-    graph: Graph<'config>,
+    graph: Option<Graph<'config>>,
     success: bool,
 }
 
 impl<'c> Controller<'c> {
     pub fn new(config: &'c Config) -> Self {
-        let graph = Graph::new();
         Self {
             config,
-            graph,
+            graph: None,
             success: true,
         }
     }
 
-    pub fn load(&mut self) -> Result<()> {
-        for (id, ac) in &self.config.artefacts {
-            let ac = ac.to_artefact_config()?;
-            let a = Artefact::new(id.as_str(), ac);
-            self.graph.add_artefact(a)?;
+    pub fn load(&mut self) -> Result<&Graph> {
+        if self.graph.is_none() {
+            let mut graph = Graph::new();
+
+            for (id, ac) in &self.config.artefact {
+                let ac = ac.to_artefact_config()?;
+                let a = Artefact::new(id.as_str(), ac);
+                graph.add_artefact(a)?;
+            }
+
+            for tc in &self.config.trace {
+                graph.add_edge_group(&tc.upper, tc.lower.iter())?;
+            }
+
+            self.graph = Some(graph);
         }
-        Ok(())
+
+        Ok(&self.graph.as_ref().unwrap())
     }
 
     pub fn find_job(&self, job: &str) -> Option<Job> {
-        Some(self.config.jobs.as_ref()?.get(job)?.clone())
+        Some(self.config.job.as_ref()?.get(job)?.clone())
     }
 
     pub fn run(&mut self, job: &Job) -> Result<()> {
+        let graph = self.load()?;
         let stdout = io::stdout();
-        let mut stdout = stdout.lock();
-        let stderr = io::stderr();
-        let mut stderr = stderr.lock();
+        let mut out: Box<dyn io::Write>;
 
-        self.success = false;
-        match &job.query {
-            Query::ValidateGraph => {}
-            Query::CacheStatus => {}
-            Query::Parse { artefacts: _ } => {}
-            Query::Trace => {
-                let r = self.graph.get_all_reqs();
-                formatters::requirements(r, &job.format, &mut stdout)?;
-                let e = self.graph.get_parsing_errors();
-                formatters::errors(e, &job.format, &mut stderr)?;
-            }
-            Query::ShowRequirement { id: _ } => {}
-            Query::ShowRequirementImpact { id: _ } => {}
+        if job.file.as_os_str() == "-" {
+            out = Box::new(stdout.lock());
+        } else {
+            let file = File::create(&job.file).map_err(|e| Error::IoError(job.file.clone(), e))?;
+            out = Box::new(file);
         }
+
+        let mut success = true;
+
+        let write_res = match &job.query {
+            Query::Trace => {
+                let t = graph.trace()?;
+                if !t.errors().is_empty() {
+                    success = false;
+                }
+                formatters::tracing(&t, &graph, &job.format, &mut out)
+            }
+            Query::Parse => {
+                let reqs = graph.get_all_reqs();
+                if let Some(_) = graph.get_parsing_errors().next() {
+                    success = false;
+                }
+                formatters::requirements(reqs, &job.format, &mut out)
+            }
+            Query::ShowRequirement { id: _ } => todo!(),
+            Query::ShowRequirementImpact { id: _ } => todo!(),
+            Query::ValidateGraph => todo!(),
+            Query::CacheStatus => todo!(),
+            Query::ParseArtefacts { artefacts: _ } => todo!(),
+        };
+
+        write_res.map_err(|e| IoError(PathBuf::from("/dev/stdout"), e))?;
+
+        self.success = success;
 
         Ok(())
     }
