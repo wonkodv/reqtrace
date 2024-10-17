99efc547843257c64138ab9dda0c2bbba13053b4
Wonko <wonko@hanstool.org>
2024-10-17 22:13:24 +0200

    nix: add foreEachSystem

--- flake.nix
+++ flake.nix
@@ -1,32 +1,35 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils?ref=c1dfcf08411b08f6b8615f7d8971a2bfa81d5e8a";
   };
 
   outputs =
-    { self
-    , nixpkgs
+    {
+      self,
+      nixpkgs,
+      flake-utils,
     }:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-      deps = with pkgs; [
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        deps = with pkgs; [
           cargo
           git
-      ];
-      devDeps = with pkgs; [
-        cargo-expand
-        clippy
-        rustc
-        rustfmt
-
-        rust-analyzer
-        pre-commit
-      ];
-    in
-    {
-      devShells.${system}.default = pkgs.mkShell {
-        buildInputs = deps ++ devDeps;
-      };
-    };
+        ];
+        devDeps = with pkgs; [
+          cargo-expand
+          clippy
+          rustc
+          rustfmt
+          rust-analyzer
+          pre-commit
+        ];
+      in
+      {
+        devShells.default = pkgs.mkShell { buildInputs = deps ++ devDeps; };
+        formatter = pkgs.nixpkgs-fmt;
+      }
+    );
 }
