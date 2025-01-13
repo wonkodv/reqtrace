4d9d7e49634cd29ca4c31b67a0d764e4ec443ce9
Wonko <wonko@hanstool.org>
2025-01-13 22:18:24 +0100

    flake: build the app

--- flake.nix
+++ flake.nix
@@ -1,4 +1,7 @@
 {
+
+  description = "A slim Requirement Tracer";
+
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils?ref=c1dfcf08411b08f6b8615f7d8971a2bfa81d5e8a";
@@ -31,77 +34,25 @@
       in
       {
 
-        packages.default = pkgs.mkDerivation {
+        packages.default = pkgs.rustPlatform.buildRustPackage {
           pname = "reqtrace";
-          version = "0.1.0";
+          version = "0.2.0";  # keep in synch with Cargo.toml version
           src = ./.;
 
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+          };
+
           buildInputs = buildDeps;
+
+          meta = with pkgs.lib; {
+            description = "A slim Requirement Tracer";
+            homepage = "https://github.com/wonkodv/reqtrace";
+          };
         };
 
         devShells.default = pkgs.mkShell { buildInputs = deps ++ buildDeps; };
 
-        checks = {
-          test =
-            pkgs.runCommand "test"
-              {
-                nativeBuildInputs = [ pkgs.cargo ];
-                src = self;
-              }
-              ''
-                cargo test
-              '';
-        };
-
-        #       checks = {
-        #         lint =
-        #           pkgs.runCommand "lint"
-        #             {
-        #               nativeBuildInputs = [
-        #                 pkgs.cargo
-        #                 pkgs.clippy
-        #               ];
-        #               src = ./.;
-        #             }
-        #             ''
-        #               cargo clippy
-        #               mkdir $out
-        #             '';
-        #
-        ##         test =
-        #           pkgs.runCommand "test"
-        #             {
-        #               nativeBuildInputs = [ pkgs.cargo ];
-        #               src = ./.;
-        #             }
-        #             ''
-        #               cargo test
-        #             '';
-        #
-        #         tmx = pkgs.runCommand "test"
-        #             {
-        #               nativeBuildInputs = [ pkgs.cargo ];
-        #               src = ./.;
-        #             }
-        #             ''
-        #               cargo run tmx
-        #             '';
-        #
-        #         format =
-        #           pkgs.runCommand "format"
-        #             {
-        #               nativeBuildInputs = [
-        #                 pkgs.nixfmt-rfc-style
-        #                 pkgs.cargo
-        #                 pkgs.rustfmt
-        #               ];
-        #               src = ./.;
-        #             }
-        #             ''
-        #               nixfmt *.nix
-        #               cargo fmt
-        #             '';
-        #       };
       }
     );
 }
